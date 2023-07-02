a77ca99cd421e8edf8b6af4b4fb79e9681c6ca3e
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-02 18:49:07 +0600

    Shop page prouduct display complete

--- src/pages/Public/Shop/Shop.jsx
+++ src/pages/Public/Shop/Shop.jsx
@@ -1,7 +1,21 @@
 import MenuCover from "../../../components/MenuCover";
 import banner from "../../../assets/images/banner.jpg";
+import { useEffect, useState } from "react";
+import ProductCard from "../../../components/ProductCard";
+
+// TODO: Filtering, Searching, Pagination
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <main>
       <MenuCover
@@ -9,6 +23,13 @@ const Shop = () => {
         subheading="Get your favorite product"
         backgroundURL={banner}
       ></MenuCover>
+      <section>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-20 gap-5 p-5">
+          {products.map((product) => (
+            <ProductCard key={product._id} productInfo={product}></ProductCard>
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
