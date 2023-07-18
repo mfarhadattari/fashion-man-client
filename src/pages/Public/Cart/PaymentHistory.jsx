622483c22430f2167d63f9639bec28d6ef760f3c
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-18 20:19:18 +0600

    payment history display in ui

--- src/pages/Public/Cart/PaymentHistory.jsx
+++ src/pages/Public/Cart/PaymentHistory.jsx
@@ -1,6 +1,7 @@
 import Loaders from "../../../components/Loaders";
 import NoData from "../../../components/NoData";
 import SectionTitle from "../../../components/SectionTitle";
+import { formatTimeDate } from "../../../utils/utils";
 import useAxiosSecure from "./../../../hooks/useAxiosSecure";
 import { useQuery } from "react-query";
 
@@ -13,11 +14,13 @@ const PaymentHistory = () => {
       return res.data;
     },
   });
-  console.log(payments);
   return (
     <main>
       <section className="my-10">
-        <SectionTitle title="Payment History" subtitle="See Your Payment History!" />
+        <SectionTitle
+          title="Payment History"
+          subtitle="See Your Payment History!"
+        />
       </section>
       {isLoading ? (
         <div className="loader-container">
@@ -27,9 +30,34 @@ const PaymentHistory = () => {
         <NoData />
       ) : (
         <section className="my-10 lg:w-3/4 mx-auto">
-          {
-            payments.length
-          }
+          <div className="overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th className="text-center">Transaction ID</th>
+                  <th className="text-center">Amount</th>
+                  <th className="text-center">Time</th>
+                  <th className="text-center">Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {payments.map((payment, idx) => (
+                  <tr key={payment._id}>
+                    <th>{idx + 1}</th>
+                    <td className="text-center space-y-1">{payment.tran_id}</td>
+                    <td className="text-center space-y-1">{payment.amount}</td>
+                    <td className="text-center space-y-1">
+                      {formatTimeDate(payment.timeDate).time}
+                    </td>
+                    <td className="text-center space-y-1">
+                      {formatTimeDate(payment.timeDate).date}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </section>
       )}
     </main>
