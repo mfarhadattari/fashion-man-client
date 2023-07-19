3aa8b8500de04186c4ffc3952121d95213abb026
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-19 12:27:23 +0600

    load user overview from sever and display loader if isLoading

--- src/pages/Public/UserHome/UserHomePage.jsx
+++ src/pages/Public/UserHome/UserHomePage.jsx
@@ -1,11 +1,31 @@
 import SectionTitle from "../../../components/SectionTitle";
+import { useQuery } from "react-query";
+import useAxiosSecure from "./../../../hooks/useAxiosSecure";
+import Loaders from "../../../components/Loaders";
 
 const UserHomePage = () => {
+  const { axiosSecure } = useAxiosSecure();
+  const { data: overview = {}, isLoading } = useQuery({
+    queryKey: ["overview", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/my-overview");
+      return res.data;
+    },
+  });
+
+  console.log(overview);
   return (
     <main>
       <section className="my-10">
         <SectionTitle title="User Home" subtitle="See Overview!" />
       </section>
+      {isLoading ? (
+        <div className="loader-container">
+          <Loaders />
+        </div>
+      ) : (
+        <section></section>
+      )}
     </main>
   );
 };
