45676ef75782ebc9773c9cb98f5fb1fa24a80351
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-29 23:28:58 +0600

    load payment information from server

--- src/pages/admin/Payments/AllPaymentPage.jsx
+++ src/pages/admin/Payments/AllPaymentPage.jsx
@@ -1,11 +1,31 @@
 import SectionTitle from "../../../components/SectionTitle";
+import useAxiosSecure from "./../../../hooks/useAxiosSecure";
+import { useQuery } from "react-query";
+import Loaders from "./../../../components/Loaders";
 
 const AllPaymentPage = () => {
+  const { axiosSecure } = useAxiosSecure();
+
+  const { data: payments = [], isLoading } = useQuery({
+    queryKey: ["payments", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin/all-payment");
+      return res.data;
+    },
+  });
+
   return (
     <main>
       <section className="my-10">
         <SectionTitle title="All Payments" subtitle="See all payment data!" />
       </section>
+      {isLoading ? (
+        <div>
+          <Loaders />
+        </div>
+      ) : (
+        <section>{payments.length}</section>
+      )}
     </main>
   );
 };
