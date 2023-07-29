df55adf24bd7194dba24ba2d5320f63df6e2872c
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-29 23:01:58 +0600

    all order data load from server

--- src/pages/admin/Orders/AllOrderPage.jsx
+++ src/pages/admin/Orders/AllOrderPage.jsx
@@ -1,6 +1,18 @@
+import { useQuery } from "react-query";
 import SectionTitle from "../../../components/SectionTitle";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Loaders from "../../../components/Loaders";
 
 const AllOrderPage = () => {
+  const { axiosSecure } = useAxiosSecure();
+  const { data: orders = [], isLoading } = useQuery({
+    queryKey: ["orders", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin/all-order");
+      return res.data;
+    },
+  });
+
   return (
     <main>
       <section className="my-10">
@@ -9,6 +21,13 @@ const AllOrderPage = () => {
           subtitle="Here are order from customer!"
         />
       </section>
+      {isLoading ? (
+        <div className="loader-container">
+          <Loaders />
+        </div>
+      ) : (
+        <section className="my-10">{orders.length}</section>
+      )}
     </main>
   );
 };
