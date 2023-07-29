ba9e8e2ed2548d588a0e2aa797309fd75112b8ae
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-29 22:18:18 +0600

    load customer data from server

--- src/pages/admin/Customer/AllCustomerPage.jsx
+++ src/pages/admin/Customer/AllCustomerPage.jsx
@@ -1,10 +1,29 @@
+import { useQuery } from "react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import SectionTitle from "./../../../components/SectionTitle";
+import Loaders from "../../../components/Loaders";
 const AllCustomerPage = () => {
+  const { axiosSecure } = useAxiosSecure();
+  const { data: customers = [], isLoading } = useQuery({
+    queryKey: ["customer", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin/all-customer");
+      return res.data;
+    },
+  });
+
   return (
     <main>
       <section className="my-10">
         <SectionTitle title="All Customer" subtitle="See our customer!" />
       </section>
+      {isLoading ? (
+        <div className="loader-container">
+          <Loaders />
+        </div>
+      ) : (
+        <section className="my-10"></section>
+      )}
     </main>
   );
 };
