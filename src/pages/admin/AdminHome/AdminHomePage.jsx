811e18ea2192eae63e5e11f90ac48a6320af7cf5
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-30 11:17:05 +0600

    Load overview data from server

--- src/pages/admin/AdminHome/AdminHomePage.jsx
+++ src/pages/admin/AdminHome/AdminHomePage.jsx
@@ -1,5 +1,19 @@
 import SectionTitle from "./../../../components/SectionTitle";
+import useAxiosSecure from "./../../../hooks/useAxiosSecure";
+import { useQuery } from "react-query";
+import Loaders from "./../../../components/Loaders";
 const AdminHomePage = () => {
+  const { axiosSecure } = useAxiosSecure();
+  const { data: adminOverview, isLoading } = useQuery({
+    queryKey: ["adminOverview", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get('admin/overview');
+      return res.data;
+    },
+  });
+
+  console.log(adminOverview);
+
   return (
     <main>
       <section className="my-10">
@@ -8,6 +22,13 @@ const AdminHomePage = () => {
           subtitle="Discover & Manage Website!"
         />
       </section>
+      {isLoading ? (
+        <div className="loader-container">
+          <Loaders />
+        </div>
+      ) : (
+        <section></section>
+      )}
     </main>
   );
 };
