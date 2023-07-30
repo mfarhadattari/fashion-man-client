eaff85a33a79b53d66736ed2ecd12602b0af811b
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-30 23:35:29 +0600

    Order details data load from server

--- src/pages/admin/Orders/OrderDetailsPage.jsx
+++ src/pages/admin/Orders/OrderDetailsPage.jsx
@@ -1,15 +1,33 @@
 import { useParams } from "react-router-dom";
 import PageTitle from "./../../../components/PageTitle";
 import SectionTitle from "./../../../components/SectionTitle";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "react-query";
+import Loaders from "../../../components/Loaders";
 const OrderDetailsPage = () => {
   const { id } = useParams();
+  const { axiosSecure } = useAxiosSecure();
+  const { data: orderDetails = {}, isLoading } = useQuery({
+    queryKey: ["orderDetails", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/order/${id}`);
+      return res.data;
+    },
+  });
+  console.log(orderDetails);
   return (
     <main className="mb-20">
       <PageTitle title="Order Details | Programmer Fashion" />
       <section className="my-10">
         <SectionTitle title="Order Details" subtitle="See order info here!" />
       </section>
-      {id}
+      {isLoading ? (
+        <div className="loader-container">
+          <Loaders />
+        </div>
+      ) : (
+        <section></section>
+      )}
     </main>
   );
 };
