0f8713b4867f7ba0898a71be28c486f9497671de
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-01 22:38:51 +0600

    customer info and order info displayed in order details page

--- src/pages/admin/Orders/OrderDetailsPage.jsx
+++ src/pages/admin/Orders/OrderDetailsPage.jsx
@@ -4,6 +4,15 @@ import SectionTitle from "./../../../components/SectionTitle";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "react-query";
 import Loaders from "../../../components/Loaders";
+import {
+  FaCheckDouble,
+  FaEnvelope,
+  FaLocationArrow,
+  FaPhone,
+  FaUser,
+  FaWallet,
+} from "react-icons/fa";
+
 const OrderDetailsPage = () => {
   const { id } = useParams();
   const { axiosSecure } = useAxiosSecure();
@@ -26,7 +35,46 @@ const OrderDetailsPage = () => {
           <Loaders />
         </div>
       ) : (
-        <section></section>
+        <section className="grid grid-cols-1 md:grid-cols-2 md:w-3/4 gap-5  mx-auto">
+          <div className="flex justify-center md:justify-start">
+            <div className="w-fit text-lg border p-5 rounded-lg hover:shadow-2xl">
+              <h4 className="text-center uppercase font-semibold text-base">
+                Cusomer Info
+              </h4>
+              <h1 className="flex gap-2 items-center">
+                <FaUser /> {orderDetails.name}
+              </h1>
+              <h1 className="flex gap-2 items-center">
+                <FaEnvelope /> {orderDetails.email}
+              </h1>
+              <h1 className="flex gap-2 items-center">
+                <FaPhone /> {orderDetails.phone}
+              </h1>
+            </div>
+          </div>
+          <div className="flex justify-center md:justify-end">
+            <div className="w-fit text-lg border p-5 rounded-lg hover:shadow-2xl">
+              <h4 className="text-center uppercase font-semibold text-base">
+                Order Info
+              </h4>
+              <div className="flex gap-2 items-center">
+                <FaLocationArrow />
+                <div>
+                  <p>{orderDetails.address}</p>
+                  <p>
+                    {orderDetails.city}, {orderDetails.postCode}
+                  </p>
+                </div>
+              </div>
+              <p className="flex gap-2 items-center">
+                <FaWallet /> Total: {orderDetails.totalAmount} &#2547;
+              </p>
+              <p className="flex gap-2 items-center">
+                <FaCheckDouble/> Status: {orderDetails.status}
+              </p>
+            </div>
+          </div>
+        </section>
       )}
     </main>
   );
