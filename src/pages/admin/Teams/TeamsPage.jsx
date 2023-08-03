10a8c4cd190989bf3498fe99cb2ef97662e8df10
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-03 23:12:56 +0600

    load team member data from server

--- src/pages/admin/Teams/TeamsPage.jsx
+++ src/pages/admin/Teams/TeamsPage.jsx
@@ -1,12 +1,27 @@
 import PageTitle from "../../../components/PageTitle";
 import SectionTitle from "../../../components/SectionTitle";
+import { useQuery } from "react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const TeamPage = () => {
+  const { axiosSecure } = useAxiosSecure();
+
+  const { data: members = [], isLoading } = useQuery({
+    queryKey: ["members", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin/team-members");
+      return res.data;
+    },
+  });
+
   return (
     <main>
       <PageTitle title="Our Team Members | Programmer Fashion" />
       <section className="my-10">
         <SectionTitle title="Our Team Members" />
+      </section>
+      <section>
+        {members.length}
       </section>
     </main>
   );
