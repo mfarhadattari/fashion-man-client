2504f9db7c75bf3dee70bff020c2f54c0c8d95d1
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-06 13:09:31 +0600

    add 3 banner

--- src/pages/Public/Home/Sections/Banner.jsx
+++ src/pages/Public/Home/Sections/Banner.jsx
@@ -4,6 +4,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper";
 import banner from "../../../../assets/images/banner.jpg";
+import banner2 from "../../../../assets/images/banner2.jpg";
+import banner3 from "../../../../assets/images/banner3.jpg";
 import { Link } from "react-router-dom";
 
 const Banner = () => {
@@ -39,11 +41,11 @@ const Banner = () => {
         </SwiperSlide>
         <SwiperSlide>
           <div className="relative">
-            <img src={banner} className="max-h-[500px] w-full" alt="" />
+            <img src={banner2} className="max-h-[500px] w-full" alt="" />
             <div className="w-full h-full absolute top-0 bg-black bg-opacity-50"></div>
             <div className="absolute top-1/3 right-20 text-white">
               <h1 className="text-2xl md:text-6xl">
-                Stylist picks <br /> beat the heat
+                Fashion up <br /> your code
               </h1>
               <Link
                 to="/shop"
@@ -56,28 +58,11 @@ const Banner = () => {
         </SwiperSlide>
         <SwiperSlide>
           <div className="relative">
-            <img src={banner} className="max-h-[500px] w-full" alt="" />
+            <img src={banner3} className="max-h-[500px] w-full" alt="" />
             <div className="w-full h-full absolute top-0 bg-black bg-opacity-50"></div>
             <div className="absolute top-1/3 right-20 text-white">
               <h1 className="text-2xl md:text-6xl">
-                Stylist picks <br /> beat the heat
-              </h1>
-              <Link
-                to="/shop"
-                className="btn btn-sm md:btn-md rounded-none md:mt-5"
-              >
-                Shop Now
-              </Link>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative">
-            <img src={banner} className="max-h-[500px] w-full" alt="" />
-            <div className="w-full h-full absolute top-0 bg-black bg-opacity-50"></div>
-            <div className="absolute top-1/3 right-20 text-white">
-              <h1 className="text-2xl md:text-6xl">
-                Stylist picks <br /> beat the heat
+                Dressing the <br /> Digital Devs
               </h1>
               <Link
                 to="/shop"
